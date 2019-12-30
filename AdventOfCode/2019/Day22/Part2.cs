92d4a720071dde0a0e9d6e18e2aaa06f0a9e72e2
Joachim Lundgren <jlu@combination.se>
2019-12-30 21:17:39 +0100

    Solved day22 part 2

--- AdventOfCode/2019/Day22/Part2.cs
+++ AdventOfCode/2019/Day22/Part2.cs
@@ -16,53 +16,42 @@ namespace AdventOfCode2019.Day22
         {
             var input = File.ReadAllLines("2019/Day22/Input.txt");
 
-            var position = 5755; //2020L;
-            var size = 10007; // 119315717514047;
-            var shuffleTimes = 2; // 101741582076661;
+            BigInteger position = 2020;
+            BigInteger size = 119315717514047;
+            BigInteger shuffleTimes = 101741582076661;
 
-            var newPos = Shuffle(input, position, size);
-            var newPos2 = Shuffle(input, newPos, size);
-            var deltapos = newPos - position;
+            BigInteger offsetPerIncrement = 0;
+            BigInteger multiplyerPerIncrement = 1;
 
-            
-            //var biggerPosition = bigPosition + deltapos * shuffleTimes;
-            var test = new BigInteger(position) + new BigInteger(deltapos) * new BigInteger(shuffleTimes) % size;
-
-            //bigPosition = (bigPosition + deltapos * shuffleTimes) % size;
-
-
-            Console.WriteLine(test); //85171953555746, 93867250878806 too high
-                                     //86997529434425
-        }
-
-        private long Shuffle(string[] input, long position, long size)
-        {
-            foreach (var line in input.Reverse())
+            foreach (var line in input)
             {
                 if (line.Equals("deal into new stack"))
                 {
-                    position = size - position - 1;
+                    multiplyerPerIncrement *= -1;
+                    offsetPerIncrement += multiplyerPerIncrement;
                 }
                 else if (line.StartsWith("deal with increment"))
                 {
-                    var increment = long.Parse(line.Substring(20));
-
-                    var n = 1;
-                    while ((n * size + position) % increment != 0)
-                        n++;
-
-                    position = (n * size + position) / increment;
+                    var dealIncrement = long.Parse(line.Substring(20));
+                    multiplyerPerIncrement *= BigInteger.ModPow(dealIncrement, size - 2, size);
                 }
                 else if (line.StartsWith("cut"))
                 {
                     var cut = long.Parse(line.Substring(4));
-                    if (cut < 0)
-                        cut = size + cut;
-
-                    position = (position + cut) % size;
+                    offsetPerIncrement += cut * multiplyerPerIncrement;
                 }
+
+                multiplyerPerIncrement = (multiplyerPerIncrement % size + size) % size;
+                offsetPerIncrement = (offsetPerIncrement % size + size) % size;
             }
-            return position;
+
+            var increment = BigInteger.ModPow(multiplyerPerIncrement, shuffleTimes, size);
+            var offset = offsetPerIncrement * (1 - increment) * BigInteger.ModPow(((1 - multiplyerPerIncrement) % size), size - 2, size);
+            offset %= size;
+
+            var result = (offset + position * increment) % size;
+
+            Console.WriteLine(result);
         }
     }
 }
